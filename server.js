43ba2745cb53e2ce012d6071e9314ce2c655f828
boy-mom <100622402+xoerycaxo@users.noreply.github.com>
2022-08-26 15:33:59 -0400

    parameters

--- server.js
+++ server.js
@@ -41,6 +41,10 @@ function filterByQuery(query, animalsArray) {
     // return the filtered results:
     return filteredResults;
   }
+  function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -48,6 +52,10 @@ app.get('/api/animals', (req, res) => {
         results = filterByQuery(req.query, results);
       }
     res.json(results);
+  });
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+      res.json(result);
   });
   app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
