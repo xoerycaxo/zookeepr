840ed7c1009ad4c3ca53cdfee55cfa118dbe0a2f
boy-mom <100622402+xoerycaxo@users.noreply.github.com>
2022-08-30 14:16:06 -0400

    front end attatched

--- server.js
+++ server.js
@@ -66,6 +66,8 @@ function filterByQuery(query, animalsArray) {
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+// to link css and jave and html files
+app.use(express.static('public'));
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -110,7 +112,19 @@ app.get('/api/animals', (req, res) => {
     }
     return true;
   }
-
+// these link thw html pages to the other pages
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+  app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+  app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
 
   app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
